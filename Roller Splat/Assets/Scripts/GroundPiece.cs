ece81ae1c32b34709f5dbc8719142d45ca56a930
Luis <luisgomezruiz@gmail.com>
2019-08-07 12:26:07 +0200

    Creating Level

---
+++ Roller Splat/Assets/Scripts/GroundPiece.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundPiece : MonoBehaviour
+{
+    /**********************************************************************************************/
+    /* Private fields                                                                             */
+    /**********************************************************************************************/
+
+    #region Private fields
+
+    [SerializeField]
+    private bool isColored = false;
+
+    #endregion // Private fields
+
+
+    /**********************************************************************************************/
+    /* Public methods                                                                             */
+    /**********************************************************************************************/
+
+    #region Public methods
+
+    public void ChangeColor(Color color)
+    {
+        GetComponent<MeshRenderer>().material.color = color;
+        isColored = true;
+    }
+
+
+    #endregion // Public methods
+}
