14807c84f3bda5cf9b7eec77af36003bd860c790
Luis <luisgomezruiz@gmail.com>
2019-08-08 21:06:45 +0200

    New levels and menu

--- Roller Splat/Assets/Scripts/GameManager.cs
+++ Roller Splat/Assets/Scripts/GameManager.cs
@@ -75,7 +75,7 @@ public class GameManager : MonoBehaviour
 
     private void NextLevel()
     {
-        if (SceneManager.GetActiveScene().buildIndex == 1)//Check if all levels have been completed
+        if (SceneManager.GetActiveScene().buildIndex == 5)//Check if all levels have been completed
         {
             SceneManager.LoadScene(0);
         }
@@ -107,6 +107,27 @@ public class GameManager : MonoBehaviour
         {
             NextLevel();
         }
+    }
+
+    public void PlayLevel1()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void PlayLevel2()
+    {
+        SceneManager.LoadScene(2);
+    }
+    public void PlayLevel3()
+    {
+        SceneManager.LoadScene(3);
+    }
+    public void PlayLevel4()
+    {
+        SceneManager.LoadScene(4);
+    }
+    public void PlayLevel5()
+    {
+        SceneManager.LoadScene(5);
     }
 
     #endregion // Public methods
